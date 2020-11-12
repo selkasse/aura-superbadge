a471740350a3b970f1f692416b94b9dce2df7790
selkasse <selkasse@gmu.edu>
2020-11-11 21:48:34 -0500

    get controller working again

--- force-app/main/default/aura/BoatSearchResults/BoatSearchResultsController.js
+++ force-app/main/default/aura/BoatSearchResults/BoatSearchResultsController.js
@@ -1,5 +1,17 @@
 ({
-    myAction : function(component, event, helper) {
+  doInit: function (component, event, helper) {
+    var action = component.get("c.getBoats");
 
-    }
-})
+    action.setCallback(this, function (response) {
+      var state = response.getState();
+
+      if (state === "SUCCESS") {
+        component.set("v.boats", response.getReturnValue());
+      } else {
+        console.log("Failed with state: " + state);
+      }
+    });
+    // Send action off to be executed
+    $A.enqueueAction(action);
+  }
+});
