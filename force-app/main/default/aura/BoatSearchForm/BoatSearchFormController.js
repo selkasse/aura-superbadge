9cb9942d54c4f6bfcc3d33387307c63788737835
selkasse <selkasse@gmu.edu>
2020-11-11 19:53:37 -0500

    transferred challenge 2 completion to main account

---
+++ force-app/main/default/aura/BoatSearchForm/BoatSearchFormController.js
@@ -0,0 +1,46 @@
+({
+  doInit: function (component, event, helper) {
+    var action = component.get("c.getBoatTypes");
+
+    action.setCallback(this, function (response) {
+      var state = response.getState();
+
+      if (state === "SUCCESS") {
+        component.set("v.boatTypes", response.getReturnValue());
+      } else {
+        console.log("Failed with state: " + state);
+      }
+    });
+    // Send action off to be executed
+    $A.enqueueAction(action);
+
+    var createRecordEvent = $A.get("e.force:createRecord");
+
+    if (createRecordEvent) {
+      console.log("passed check");
+      component.set("v.showNewButton", true);
+    } else {
+      console.log("did not pass check");
+    }
+  },
+  boatTypeChange: function (component, event, helper) {
+    var boatType = event.getSource().get("v.value");
+
+    component.set("v.boatType", boatType);
+  },
+  clickNew: function (component, event, helper) {
+    var createRecordEvent = $A.get("e.force:createRecord");
+
+    if (createRecordEvent) {
+      var params = { entityApiName: "Boat__c" };
+      var boatType = component.get("v.boatType");
+
+      if (boatType) {
+        params.defaultFieldValues = { BoatType__c: boatType };
+      }
+
+      createRecordEvent.setParams(params);
+      createRecordEvent.fire();
+    }
+  }
+});
---
+++ force-app/main/default/aura/FriendswithBoats/FriendswithBoats.app
@@ -0,0 +1,8 @@
+<aura:application extends="force:slds">
+  <lightning:layout>
+    <lightning:layoutItem size="8">
+      <c:BoatSearch />
+    </lightning:layoutItem>
+    <lightning:layoutItem size="4"> </lightning:layoutItem>
+  </lightning:layout>
+</aura:application>
